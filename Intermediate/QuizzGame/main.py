bfa2fa80544e80a41fe0bb96079088aa65a7deac
Henri PETERS <henri.peters@eu.corp.airliquide.com>
2023-05-17 17:56:03 +0800

    incroyable

--- Intermediate/QuizzGame/main.py
+++ Intermediate/QuizzGame/main.py
@@ -1,4 +1,5 @@
 import random
+import data
 class Question:
     def __init__(self, prompt, answer):
         print("Question is : " + prompt)
@@ -7,7 +8,29 @@ class Question:
     def addrandomanswer(self):
         self.answer = random.random()
 
-question=Question("Comment je m'appelle ?","Henri")
-print(question.answer)
-question.addrandomanswer()
-print(question.answer)
+class quizz:
+    def __init__(self,questionlist):
+        self.questionlist = questionlist
+        self.score = 0
+    def nextquestion(self):
+        id = int(random.uniform(0,len(data.question_data)))
+        self.questionlist.append=Question(question["text"],question["answer"])
+
+questionlist = []
+for question in data.question_data:
+    questionlist.append=Question(question["text"],question["answer"])
+
+
+    
+
+play = True
+while play:
+    id = int(random.uniform(0,len(data.question_data)))
+    
+    myanswer =""
+    while myanswer != "True" and myanswer != "False":
+        myanswer = input("Your answer is : ")
+    if myanswer == question.answer:
+        print(question.answer)
+    else:
+        play = False
