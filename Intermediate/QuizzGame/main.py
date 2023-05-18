e0d39e46a76ee593126eaec9947fc84113476c99
Henri PETERS <henri.peters@hotmail.fr>
2023-05-18 09:42:33 +0800

    quizz class

--- Intermediate/QuizzGame/main.py
+++ Intermediate/QuizzGame/main.py
@@ -1,36 +1,22 @@
 import random
 import data
-class Question:
-    def __init__(self, prompt, answer):
-        print("Question is : " + prompt)
-        self.prompt = prompt
-        self.answer = answer
-    def addrandomanswer(self):
-        self.answer = random.random()
-
-class quizz:
-    def __init__(self,questionlist):
-        self.questionlist = questionlist
-        self.score = 0
-    def nextquestion(self):
-        id = int(random.uniform(0,len(data.question_data)))
-        self.questionlist.append=Question(question["text"],question["answer"])
+import quizzclass
 
 questionlist = []
 for question in data.question_data:
-    questionlist.append=Question(question["text"],question["answer"])
+    questionlist.append(quizzclass.Question(question["text"],question["answer"]))
 
+quizz = quizzclass.Quizz(questionlist)
 
-    
 
 play = True
 while play:
-    id = int(random.uniform(0,len(data.question_data)))
-    
+    answer = quizz.nextquestion() 
     myanswer =""
     while myanswer != "True" and myanswer != "False":
         myanswer = input("Your answer is : ")
-    if myanswer == question.answer:
-        print(question.answer)
+    if myanswer == answer:
+        print(answer)
+        quizz.addscore()
     else:
         play = False
