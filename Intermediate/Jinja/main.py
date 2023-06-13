ace66e094ecc6f5025b325fd92635e0986295b02
Henri PETERS <henri.peters@eu.corp.airliquide.com>
2023-06-13 11:41:44 +0800

    adding age prediction

--- Intermediate/Jinja/main.py
+++ Intermediate/Jinja/main.py
@@ -1,10 +1,10 @@
-from flask import Flask
-import jinja2
-from flask import render_template
+from flask import Flask, render_template
 import random
 import datetime
+import requests
+import ssl
 
-
+ssl._create_default_https_context = ssl._create_unverified_context
 app = Flask(__name__)
 
 
@@ -12,7 +12,23 @@ app = Flask(__name__)
 def hello_world():
     random_number = random.randint(1, 10)
     footer = "Copyright " + str(datetime.date.today().year) + " Henri "
-    return render_template('index.html',num=random_number,footer=footer)
+    return render_template('index.html', num=random_number, footer=footer, name=None,gender=None,age=None)
+
+
+@app.route('/guess/<name>')
+def guess(name):
+    random_number = random.randint(1, 10)
+    # get gender
+    genderurl = f"http://api.genderize.io?name={name}"
+    genderresponse = requests.get(genderurl, verify=False).json()
+    gender = genderresponse.get('gender')
+    #get age
+    age = f"http://api.agify.io?name={name}"
+    ageresponse = requests.get(age, verify=False).json()
+    age = ageresponse.get('age')
+    # footer
+    footer = "Copyright " + str(datetime.date.today().year) + " Henri "
+    return render_template('index.html', num=random_number, footer=footer, name=name, gender=gender,age=age)
 
 
 if __name__ == "__main__":
