05e1e29c893081b0a6c09eaeef10b77e8b4d850e
Henri PETERS <henri.peters@eu.corp.airliquide.com>
2023-06-13 18:00:41 +0800

    route

--- Advanced/Flask/main.py
+++ Advanced/Flask/main.py
@@ -7,6 +7,18 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/success")
+def success():
+    return render_template("success.html")
+
+@app.route("/denied")
+def denied():
+    return render_template("denied.html")
+
 
 if __name__ == '__main__':
     app.run(debug=True)
