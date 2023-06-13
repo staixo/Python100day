e9954a700509dbd1957394888fc2580568a1ebdb
Henri PETERS <henri.peters@eu.corp.airliquide.com>
2023-06-13 10:30:04 +0800

    from static site

--- Intermediate/Flask/project/server.py
+++ Intermediate/Flask/project/server.py
@@ -1,53 +1,26 @@
-from flask import Flask
-from flask import render_template
-
-
-def make_heading(func):
-    def wrapper(value):
-        return "<h1>" + str(func(value)) + "</h1>"
-    return wrapper
-
-
-def make_emphasis(func):
-    def wrapper(value):
-        return "<em>" + str(func(value)) + "</em>"
-    return wrapper
-
-
-def make_header(func):
-    def wrapper(value):
-        return "<h2>" + str(func(value)) + "</h2>"
-    return wrapper
-
-
-def make_bold(func):
-    def wrapper(value):
-        return "<strong>" + str(func(value)) + "</strong>"
-    return wrapper
-
-
-def make_underlined(func):
-    def wrapper(value):
-        return "<u>" + str(func(value)) + "</u>"
-    return wrapper
-
-
-def add_image(image_url):
-    def decorator(func):
-        def inner_wrapper(value=None):
-            return str(func(value)) + "<br><img src='" + image_url + "'/>"
-        return inner_wrapper
-    return decorator
-
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
 @app.route('/')
-def home():
+def index():
     return render_template('index.html')
 
+@app.route('/index.html')
+def index_page():
+    return render_template('index.html')
 
+@app.route('/left-sidebar.html')
+def left_sidebar():
+    return render_template('left-sidebar.html')
+
+@app.route('/no-sidebar.html')
+def no_sidebar():
+    return render_template('no-sidebar.html')
+
+@app.route('/right-sidebar.html')
+def right_sidebar():
+    return render_template('right-sidebar.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
