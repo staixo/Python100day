ae3d2dd476c3e9ecd598b768b4c4493320d229bd
Henri PETERS <henri.peters@eu.corp.airliquide.com>
2023-06-15 18:22:31 +0800

    Trop stylé !

--- Advanced/REST_API/main.py
+++ Advanced/REST_API/main.py
@@ -76,24 +76,24 @@ def get_all_cafes():
     return jsonify(cafes_list)
 
 
+
 @app.route("/add", methods=["POST"])
-def add_cafe():
+def post_new_cafe():
     new_cafe = Cafe(
-        name=request.json["name"],
-        map_url=request.json["map_url"],
-        img_url=request.json["img_url"],
-        location=request.json["location"],
-        seats=request.json["seats"],
-        has_toilet=request.json["has_toilet"],
-        has_wifi=request.json["has_wifi"],
-        has_sockets=request.json["has_sockets"],
-        can_take_calls=request.json["can_take_calls"],
-        coffee_price=request.json["coffee_price"]
+        name=request.form.get("name"),
+        map_url=request.form.get("map_url"),
+        img_url=request.form.get("img_url"),
+        location=request.form.get("location"),
+        has_sockets=bool(request.form.get("sockets")),
+        has_toilet=bool(request.form.get("toilet")),
+        has_wifi=bool(request.form.get("wifi")),
+        can_take_calls=bool(request.form.get("calls")),
+        seats=request.form.get("seats"),
+        coffee_price=request.form.get("coffee_price"),
     )
     db.session.add(new_cafe)
     db.session.commit()
-
-    return jsonify(message="Cafe added successfully.")
+    return jsonify(response={"success": "Successfully added the new cafe."})
 
 
 if __name__ == '__main__':
