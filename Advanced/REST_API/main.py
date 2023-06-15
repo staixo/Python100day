f1d4d46b283e71dc86c25eb765023f9af96e6143
Henri PETERS <henri.peters@eu.corp.airliquide.com>
2023-06-15 17:49:55 +0800

    cafe add and all

--- Advanced/REST_API/main.py
+++ Advanced/REST_API/main.py
@@ -1,15 +1,16 @@
 from flask import Flask, jsonify, render_template, request
 from flask_sqlalchemy import SQLAlchemy
+import random
 
 app = Flask(__name__)
 
-##Connect to Database
+# Connect to Database
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///cafes.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 
-##Cafe TABLE Configuration
+# Cafe TABLE Configuration
 class Cafe(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(250), unique=True, nullable=False)
@@ -24,18 +25,75 @@ class Cafe(db.Model):
     coffee_price = db.Column(db.String(250), nullable=True)
 
 
+with app.app_context():
+    db.create_all()
+
+
 @app.route("/")
 def home():
     return render_template("index.html")
-    
 
-## HTTP GET - Read Record
 
-## HTTP POST - Create Record
+@app.route("/random")
+def get_random_cafe():
+    cafes = db.session.query(Cafe).all()
+    random_cafe = random.choice(cafes)
+    return jsonify(cafe={
+        "id": random_cafe.id,
+        "name": random_cafe.name,
+        "map_url": random_cafe.map_url,
+        "img_url": random_cafe.img_url,
+        "location": random_cafe.location,
+        "seats": random_cafe.seats,
+        "has_toilet": random_cafe.has_toilet,
+        "has_wifi": random_cafe.has_wifi,
+        "has_sockets": random_cafe.has_sockets,
+        "can_take_calls": random_cafe.can_take_calls,
+        "coffee_price": random_cafe.coffee_price,
+    })
 
-## HTTP PUT/PATCH - Update Record
 
-## HTTP DELETE - Delete Record
+@app.route("/all")
+def get_all_cafes():
+    cafes = db.session.query(Cafe).all()
+    cafes_list = []
+    for cafe in cafes:
+        cafe_data = {
+            "id": cafe.id,
+            "name": cafe.name,
+            "map_url": cafe.map_url,
+            "img_url": cafe.img_url,
+            "location": cafe.location,
+            "seats": cafe.seats,
+            "has_toilet": cafe.has_toilet,
+            "has_wifi": cafe.has_wifi,
+            "has_sockets": cafe.has_sockets,
+            "can_take_calls": cafe.can_take_calls,
+            "coffee_price": cafe.coffee_price,
+        }
+        cafes_list.append(cafe_data)
+
+    return jsonify(cafes_list)
+
+
+@app.route("/add", methods=["POST"])
+def add_cafe():
+    new_cafe = Cafe(
+        name=request.json["name"],
+        map_url=request.json["map_url"],
+        img_url=request.json["img_url"],
+        location=request.json["location"],
+        seats=request.json["seats"],
+        has_toilet=request.json["has_toilet"],
+        has_wifi=request.json["has_wifi"],
+        has_sockets=request.json["has_sockets"],
+        can_take_calls=request.json["can_take_calls"],
+        coffee_price=request.json["coffee_price"]
+    )
+    db.session.add(new_cafe)
+    db.session.commit()
+
+    return jsonify(message="Cafe added successfully.")
 
 
 if __name__ == '__main__':
