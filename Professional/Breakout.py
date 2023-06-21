61fe34f3267c44081f1426b4fc799a762450c588
Henri PETERS <henri.peters@hotmail.fr>
2023-06-21 12:06:43 +0800

    Workingggg

--- Professional/Breakout.py
+++ Professional/Breakout.py
@@ -1,4 +1,5 @@
 import turtle
+import random
 
 # Set up the screen
 screen = turtle.Screen()
@@ -21,10 +22,10 @@ ball.shape("circle")
 ball.color("white")
 ball.penup()
 ball.goto(0, 0)
-ball.dx = 0.2
-ball.dy = -0.2
+ball.dx = random.choice([-2, 2])
+ball.dy = -2
 
-# Create the bricks
+# Create bricks
 bricks = []
 colors = ["red", "orange", "yellow", "green", "blue"]
 for y in range(5):
@@ -34,20 +35,20 @@ for y in range(5):
         brick.color(colors[y])
         brick.shapesize(stretch_wid=1, stretch_len=2)
         brick.penup()
-        brick.goto(x * 70, 200 - y * 30)
+        brick.goto(x * 50, 200 - y * 30)
         bricks.append(brick)
 
-# Move the paddle left
+# Function to move the paddle left
 def move_left():
     x = paddle.xcor()
-    if x > -340:
+    if x > -350:
         x -= 20
     paddle.setx(x)
 
-# Move the paddle right
+# Function to move the paddle right
 def move_right():
     x = paddle.xcor()
-    if x < 340:
+    if x < 350:
         x += 20
     paddle.setx(x)
 
@@ -64,36 +65,34 @@ while True:
     ball.setx(ball.xcor() + ball.dx)
     ball.sety(ball.ycor() + ball.dy)
 
-    # Check for ball collision with walls
+    # Check for wall collisions
     if ball.xcor() > 390 or ball.xcor() < -390:
         ball.dx *= -1
-
     if ball.ycor() > 290:
         ball.dy *= -1
 
+    # Check for paddle collision
+    if (ball.ycor() < -240) and (ball.xcor() > paddle.xcor() - 50) and (ball.xcor() < paddle.xcor() + 50):
+        ball.dy *= -1
+
+    # Check for brick collisions
+    for brick in bricks:
+        if (ball.ycor() > brick.ycor() - 20) and (ball.ycor() < brick.ycor() + 20) and (ball.xcor() > brick.xcor() - 50) and (ball.xcor() < brick.xcor() + 50):
+            bricks.remove(brick)
+            brick.goto(1000, 1000)  # Move brick off the screen
+            ball.dy *= -1
+
+    # Check if all bricks are destroyed
+    if len(bricks) == 0:
+        ball.goto(0, 0)
+        ball.dx = random.choice([-2, 2])
+        ball.dy = -2
+        for brick in bricks:
+            brick.goto(brick.xcor(), brick.ycor() - 100)
+
+    # Check for game over
     if ball.ycor() < -290:
         ball.goto(0, 0)
         ball.dy *= -1
-
-    # Check for ball collision with paddle
-    if (ball.dx > 0) and (350 > ball.xcor() > 340) and (ball.ycor() < paddle.ycor() + 10 and ball.ycor() > paddle.ycor() - 10):
-        ball.sety(paddle.ycor() + 20)
-        ball.dy *= -1
-
-    if (ball.dx < 0) and (-350 < ball.xcor() < -340) and (ball.ycor() < paddle.ycor() + 10 and ball.ycor() > paddle.ycor() - 10):
-        ball.sety(paddle.ycor() + 20)
-        ball.dy *= -1
-
-    # Check for ball collision with bricks
-    for brick in bricks:
-        if (brick.dx > 0) and (brick.xcor() + 30 > ball.xcor() > brick.xcor() - 30) and (
-                brick.ycor() + 10 > ball.ycor() > brick.ycor() - 10):
-            brick.goto(1000, 1000)  # Move brick off the screen
-            ball.dx *= -1
-            ball.dy *= -1
-
-        if (brick.dx < 0) and (brick.xcor() + 30 > ball.xcor() > brick.xcor() - 30) and (
-                brick.ycor() + 10 > ball.ycor() > brick.ycor() - 10):
-            brick.goto(1000, 1000)  # Move brick off the screen
-            ball.dx *= -1
-            ball.dy *= -1
+        for brick in bricks:
+            brick.goto(brick.xcor(), brick.ycor() - 100)
